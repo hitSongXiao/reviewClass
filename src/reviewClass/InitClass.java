3d65bf7b06c1fca27700b1a83b602ceccd037a12
xs1559474822 <15683609851@163.com>
2018-02-04 21:56:00 +0800

    newly

---
+++ src/reviewClass/InitClass.java
@@ -0,0 +1,5 @@
+package reviewClass;
+
+public class InitClass {
+
+}
--- src/reviewClass/myClass.java
+++ src/reviewClass/myClass.java
@@ -1,5 +1,10 @@
 package reviewClass;
 
 public class myClass {
-
+	float screen;
+	float cpu;
+	float mem;
+	void call(){
+		System.out.println("the phone can call,screen:"+screen+" ,cpu:"+cpu+"mem:"+mem);
+	}
 }
