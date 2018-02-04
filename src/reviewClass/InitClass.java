ac3c4f6692b21bac67aaa40437924d639353ef93
xs1559474822 <15683609851@163.com>
2018-02-04 22:43:17 +0800

    1.01

--- src/reviewClass/InitClass.java
+++ src/reviewClass/InitClass.java
@@ -1,5 +1,8 @@
 package reviewClass;
 
 public class InitClass {
-
+	public static void main(String args[]){
+		myClass newClass= new myClass();
+		newClass.call();
+	}
 }
--- src/reviewClass/myClass.java
+++ src/reviewClass/myClass.java
@@ -1,10 +1,19 @@
 package reviewClass;
-
+import java.util.Scanner;
 public class myClass {
 	float screen;
 	float cpu;
 	float mem;
 	void call(){
 		System.out.println("the phone can call,screen:"+screen+" ,cpu:"+cpu+"mem:"+mem);
+	}
+	myClass(){
+		Scanner input=new Scanner(System.in);
+		System.out.println("Please input the screen");
+		screen=input.nextFloat();
+		System.out.println("Please input the cpu");
+		cpu=input.nextFloat();
+		System.out.println("Please input the mem");
+		mem=input.nextFloat();
 	}
 }
