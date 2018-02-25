ea12617e255ab9c8d4af722aa4738400f7842094
xs1559474822 <15683609851@163.com>
2018-02-25 19:12:15 +0800

    first

--- src/reviewClass/InitClass.java
+++ src/reviewClass/InitClass.java
@@ -1,8 +1,16 @@
 package reviewClass;
-
 public class InitClass {
 	public static void main(String args[]){
 		myClass newClass= new myClass();
 		newClass.call();
+		newClass.cpu=2.0f;
+		newClass.mem=3.0f;
+		myClass.system=4;
+		newClass.call();
+		int a[]=new int[]{1,2,3};
+		for(int i :a)
+		{
+			System.out.println(i);
+		}
 	}
 }
